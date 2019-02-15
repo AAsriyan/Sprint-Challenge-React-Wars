d0c869ce1f6190e19283619a5a9ae3a4a722428e
AAsriyan <aasriyan@protonmail.com>
2019-02-15 11:29:10 -0500

    Created the necessary components and made the props drill down to each character

---
+++ starwars/src/components/Character.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Character = props => {
+  return (
+    <div className="character">
+      <h2>{props.character.name}</h2>
+    </div>
+    
+  );
+}
+
+export default Character;
